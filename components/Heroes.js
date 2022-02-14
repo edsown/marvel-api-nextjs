57cba35f2962a4aa88d09fb04035eed32b35e472
edsown <edsonoliveira.contato@gmail.com>
2022-02-13 22:59:48 -0300

    added a function to fetch more characters

--- components/Heroes.js
+++ components/Heroes.js
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "../components/Heroes.module.css";
-
+import Link from "next/link";
 function Heroes(props) {
   return (
-    <div className={styles.container}>
-      <img src={props.source}></img>
-      <div className={styles.tcontainer}>
-        <h5 className={styles.text}>{props.name}</h5>
+    <Link href={props.link}>
+      <div className={styles.container}>
+        <img src={props.source}></img>
+        <div className={styles.tcontainer}>
+          <h5 className={styles.text}>{props.name}</h5>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
